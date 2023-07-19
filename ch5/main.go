9fc225fd281b41942d5e80e1510826d0af255764
mikehsu <a3698521tw11@gmail.com>
2023-07-20 00:33:32 +0800

    doc-ch5: add golang example

---
+++ ch5/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Hello foo"})
+	})
+	router.Run()
+}
